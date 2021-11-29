65c757bb4faab4181d0e9da0ee2164784568e1e2
amaldhawan <95120682+amaldhawan@users.noreply.github.com>
2021-11-29 13:27:05 +0530

    listing the instance details
    commit

---
+++ listec2.py
@@ -0,0 +1,19 @@
+import boto3
+import sys
+
+client = boto3.client('ec2', region_name = Region, aws_access_key_id= Accesskey, aws_secret_access_key = Secretkey)
+
+mohanec2 = client.describe_instances()
+
+for mohan1 in mohanec2['Reservations']:
+
+    for mohan2 in mohan1['Instances']:
+    
+        for mohan3 in mohan2['Tags']:
+        
+			print(mohan2['InstanceId'])
+			print(mohan2['InstanceType'])
+			print(mohan2['LaunchTime'])
+			print(mohan2['State']['Name'])
+			print(mohan3['Value'])
+    
